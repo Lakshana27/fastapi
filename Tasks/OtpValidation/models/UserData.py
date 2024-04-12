9dc1696e18f40ad9d85930d6bd7f2e5a03fe627c
Lakshana27 <114004274+Lakshana27@users.noreply.github.com>
2024-04-13 00:06:38 +0530

    Update UserData.py

--- Tasks/OtpValidation/models/UserData.py
+++ Tasks/OtpValidation/models/UserData.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Integer, TIMESTAMP, func
-from OtpValidation.database_setup.database import Base
+from OtpValidation.database.database_setup import Base
 
 class UserData(Base):
     __tablename__ = "otp_table"
